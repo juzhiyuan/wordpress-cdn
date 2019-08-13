99503e5cdcef8bdad3951a5aa2b9135912e24228
Shaoyao·琚 <jjzhiyuan@gmail.com>
2019-08-13 21:44:39 +0800

    Feature Add Tencent COS (#4)
    Add Tencent COS Support

--- tencent_cos.php
+++ tencent_cos.php
@@ -4,56 +4,91 @@ require "utils.php";
 
 require_once 'sdk/tencent-cos/vendor/autoload.php';
 
-// use OSS\OssClient;
-// use OSS\Core\OssException;
+use Qcloud\Cos\Client;
 
-// function file_upload($object, $filePath)
-// {
-//   // Init OSS Client Instance
-//   $option = get_option('wordpress_oss_options');
-//   $ossClient = new OssClient($option['accessKeyId'], $option['accessKeySecret'], $option['endpoint']);
+function file_upload($object, $filePath)
+{
+  // Init OSS Client Instance
+  $option = get_option('wordpress_cdn_options');
+  $cosClient = new Client(array(
+    'region' => $option['endpoint'],
+    'credentials' => array(
+      'secretId' => $option['accessKeyId'],
+      'secretKey' => $option['accessKeySecret'],
+    ),
+  ));
 
-//   // Action
-//   try {
-//     $ossClient->uploadFile($option['bucket'], $object, $filePath);
+  // Action
+  try {
+    $cosClient->putObject(array(
+      'Bucket' => $option['bucket'],
+      'Key' => $object,
+      'Body' => fopen($filePath, 'rs'),
+    ));
 
-//     delete_local_file($filePath);
-//   } catch (OssException $e) {
-//     return FALSE;
-//   }
-// }
+    delete_local_file($filePath);
+  } catch (Exception $e) {
+    return FALSE;
+  }
+}
 
-// function check_file_exist_on_remote($object) {
-//   $option = get_option('wordpress_oss_options');
-//   $ossClient = new OssClient($option['accessKeyId'], $option['accessKeySecret'], $option['endpoint']);
-//   return $ossClient->doesObjectExist($option['bucket'], $object);
-// }
+function check_file_exist_on_remote($object)
+{
+  $option = get_option('wordpress_cdn_options');
+  $cosClient = new Client(array(
+    'region' => $option['endpoint'],
+    'credentials' => array(
+      'secretId' => $option['accessKeyId'],
+      'secretKey' => $option['accessKeySecret'],
+    ),
+  ));
 
-// // Different platform needs different deleteObjects format
-// function delete_remote_attachment($post_id)
-// {
-//   $deleteObjects = array();
-//   $meta = wp_get_attachment_metadata($post_id);
+  try {
+    $cosClient->headObject(array(
+      'Bucket' => $option['bucket'],
+      'Key' => $object,
+    ));
+    return TRUE;
+  } catch(Exception $e) {
+    return FALSE;
+  }
+}
 
-//   if (isset($meta['file'])) {
-//     $attachment_key = $meta['file'];
-//     array_push($deleteObjects, $attachment_key);
-//   } else {
-//     $file = get_attached_file($post_id);
-//     $attached_key = str_replace(wp_get_upload_dir()['basedir'] . '/', '', $file);
-//     $deleteObjects[] = $attached_key;
-//   }
+// Different platform needs different deleteObjects format
+function delete_remote_attachment($post_id)
+{
+  $deleteObjects = array();
+  $meta = wp_get_attachment_metadata($post_id);
 
-//   if (isset($meta['sizes']) && count($meta['sizes']) > 0) {
-//     foreach ($meta['sizes'] as $val) {
-//       $attachment_thumb_key = dirname($meta['file']) . '/' . $val['file'];
-//       array_push($deleteObjects, $attachment_thumb_key);
-//     }
-//   }
+  if (isset($meta['file'])) {
+    $attachment_key = $meta['file'];
+    $deleteObjects[] = array('Key' => $attachment_key);
+  } else {
+    $file = get_attached_file($post_id);
+    $attached_key = str_replace(wp_get_upload_dir()['basedir'] . '/', '', $file);
+    $deleteObjects[] = array('Key' => $attached_key);
+  }
 
-//   if (!empty($deleteObjects)) {
-//     $option = get_option('wordpress_oss_options');
-//     $ossClient = new OssClient($option['accessKeyId'], $option['accessKeySecret'], $option['endpoint']);
-//     $ossClient->deleteObjects($option['bucket'], $deleteObjects);
-//   }
-// }
+  if (isset($meta['sizes']) && count($meta['sizes']) > 0) {
+    foreach ($meta['sizes'] as $val) {
+      $attachment_thumb_key = dirname($meta['file']) . '/' . $val['file'];
+      $deleteObjects[] = array('Key' => $attachment_thumb_key);
+    }
+  }
+
+  if (!empty($deleteObjects)) {
+    $option = get_option('wordpress_cdn_options');
+    $cosClient = new Client(array(
+      'region' => $option['endpoint'],
+      'credentials' => array(
+        'secretId' => $option['accessKeyId'],
+        'secretKey' => $option['accessKeySecret'],
+      ),
+    ));
+    
+    $cosClient->deleteObjects(array(
+      'Bucket' => $option['bucket'],
+      'Objects' => $deleteObjects,
+    ));
+  }
+}
